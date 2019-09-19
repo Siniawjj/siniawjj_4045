58d72c551288292d519ff043485fc2e993b59219
Administrator <Administrator@DESKTOP-099UQRG>
2019-09-19 13:17:34 -0400

    changing start to index

--- environmentsetup/src/main/java/com/environmentsetup/EnvironmentSetupController.java
+++ environmentsetup/src/main/java/com/environmentsetup/EnvironmentSetupController.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class 
 EnvironmentSetupController {
 
-	@RequestMapping("/start")
-	public String start()
+	@RequestMapping("/index")
+	public String index()
 	{
-		return "start";
+		return "index";
 	}
 }
