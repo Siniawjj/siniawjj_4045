7330db252627c0486da58fc328559586450f5969
Administrator <Administrator@DESKTOP-099UQRG>
2019-09-19 12:49:56 -0400

    Initial Commit For Environment Setup

---
+++ environmentsetup/src/test/java/com/environmentsetup/environmentsetup/EnvironmentsetupApplicationTests.java
@@ -0,0 +1,16 @@
+package com.environmentsetup.environmentsetup;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EnvironmentsetupApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
